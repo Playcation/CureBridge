7c87285762ababc56814cfe9438b839eed27969b
kangjunhyuk <kangjun1290@gmail.com>
2025-07-25 15:54:42 +0900

    Fix : ChatMessageRepositoryJPA 상속

--- Chat/src/main/java/com/example/chat/repository/ChatMessageRepositoryJPA.java
+++ Chat/src/main/java/com/example/chat/repository/ChatMessageRepositoryJPA.java
@@ -1,10 +1,13 @@
 package com.example.chat.repository;
 
+import com.example.chat.entity.Chat;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
-@Configuration
+@Repository
 @EnableJpaRepositories(basePackages = "com.example.chat.repository.jpa")
-public class ChatMessageRepositoryJPA {
+public interface ChatMessageRepositoryJPA extends JpaRepository<Chat, String > {
 
 }
