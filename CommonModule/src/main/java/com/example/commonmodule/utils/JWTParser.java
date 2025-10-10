221501002bd56ed571fffcf88490daf7c626a01d
KM4HS <ac4hsu@gmail.com>
2025-10-10 18:35:18 +0900

    refactor: jwtUtil 책임 분리 진행중
    parser, validator, issuer로 구분할 예정

---
+++ CommonModule/src/main/java/com/example/commonmodule/utils/JWTParser.java
@@ -0,0 +1,11 @@
+package com.example.commonmodule.utils;
+
+/**
+ * <ul>
+ * <li>packageName    : com.example.commonmodule.utils
+ * <li>fileName       : JWTParser
+ * <li>date           : 25. 10. 10.
+ * <li>description    :
+ * </ul>
+ */public class JWTParser {
+}
