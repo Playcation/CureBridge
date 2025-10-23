ee979ddacc5eabccba6bb2ab75fa57e5146e8dd6
K-da-fire <tmddnr3503@gmail.com>
2025-10-23 21:41:30 +0900

    커스텀 예외 추가

--- MemberService/src/main/java/com/example/memberservice/controller/OrganizationController.java
+++ MemberService/src/main/java/com/example/memberservice/controller/OrganizationController.java
@@ -1,9 +1,17 @@
 package com.example.memberservice.controller;
 
+import com.example.commonmodule.exceptions.NoAuthorizedException;
+import com.example.commonmodule.utils.JwtParser;
 import com.example.memberservice.dto.OrgCreateRequestDto;
+import com.example.memberservice.dto.OrgManagerCreateRequestDto;
+import com.example.memberservice.dto.OrgManagerResponseDto;
 import com.example.memberservice.dto.OrgResponseDto;
 import com.example.memberservice.dto.OrgUpdateDto;
-import com.example.memberservice.security.JwtUtil;
+import com.example.memberservice.entity.Role;
+import com.example.memberservice.enums.AdminException;
+import com.example.memberservice.enums.ManagerException;
+import com.example.memberservice.security.TokenSettings;
+import com.example.memberservice.service.OrgManagerService;
 import com.example.memberservice.service.OrganizationService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +22,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,32 +32,42 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrganizationController {
 
   private final OrganizationService organizationService;
-  private final JwtUtil jwtUtil;
+  private final OrgManagerService orgManagerService;
+  private final JwtParser jwtParser;
 
   @PostMapping("/create")
   public ResponseEntity<OrgResponseDto> createOrganization(
+      @RequestHeader(TokenSettings.ACCESS_TOKEN_CATEGORY) String authorizationHeader,
       @RequestBody OrgCreateRequestDto orgCreateRequestDto
   ) {
+    checkAdmin(authorizationHeader);
     return ResponseEntity.ok().body(organizationService.createOrganization(orgCreateRequestDto));
   }
 
   @GetMapping
-  public ResponseEntity<List<OrgResponseDto>> getAllOrganization() {
+  public ResponseEntity<List<OrgResponseDto>> getAllOrganization(
+      @RequestHeader(TokenSettings.ACCESS_TOKEN_CATEGORY) String authorizationHeader
+  ) {
+    checkOrgOrAdmin(authorizationHeader);
     return ResponseEntity.ok().body(organizationService.getAllOrganization());
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<OrgResponseDto> getOrganization(
-      @PathVariable Long id
+      @PathVariable Long id,
+      @RequestHeader(TokenSettings.ACCESS_TOKEN_CATEGORY) String authorizationHeader
   ) {
+    checkOrgOrAdmin(authorizationHeader);
     return ResponseEntity.ok().body(organizationService.getOrganization(id));
   }
 
   @PatchMapping("/{id}")
   public ResponseEntity<OrgResponseDto> updateOrganization(
+      @RequestHeader(TokenSettings.ACCESS_TOKEN_CATEGORY) String authorizationHeader,
       @PathVariable Long id,
       @RequestBody OrgUpdateDto orgUpdateDto
   ) {
+    checkOrgOrAdmin(authorizationHeader);
     return ResponseEntity.ok().body(organizationService.updateOrganization(id, orgUpdateDto));
   }
 
@@ -60,8 +79,33 @@ public class OrganizationController {
   }
 
   @PostMapping("/create/employee")
-  public String createEmployee() {
-    return "hello";
+  public ResponseEntity<OrgManagerResponseDto> createEmployee(
+      @RequestHeader(TokenSettings.ACCESS_TOKEN_CATEGORY) String authorizationHeader,
+      @RequestBody OrgManagerCreateRequestDto orgManagerCreateRequestDto
+  ) {
+    checkOrg(authorizationHeader);
+    return ResponseEntity.ok().body(orgManagerService.createOrgManager(orgManagerCreateRequestDto));
+  }
+
+  private void checkAdmin(String authorizationHeader) {
+    String role = jwtParser.parseRole(authorizationHeader);
+    if (!Role.ORG_MANAGER.toString().equals(role)) {
+      throw new NoAuthorizedException(ManagerException.NO_AUTHORIZED_MANAGER);
+    }
+  }
+
+  private void checkOrg(String authorizationHeader) {
+    String role = jwtParser.parseRole(authorizationHeader);
+    if (!Role.ORG_ADMIN.toString().equals(role)) {
+      throw new NoAuthorizedException(AdminException.NO_AUTHORIZED_ADMIN);
+    }
+  }
+
+  private void checkOrgOrAdmin(String authorizationHeader) {
+    String role = jwtParser.parseRole(authorizationHeader);
+    if (!Role.ORG_MANAGER.toString().equals(role) || !Role.ORG_ADMIN.toString().equals(role)) {
+      throw new NoAuthorizedException(ManagerException.NO_AUTHORIZED_MANAGER);
+    }
   }
 
 }
