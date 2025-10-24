745625a9797fc51da5499fe036ce4fcbbd226b73
K-da-fire <tmddnr3503@gmail.com>
2025-10-24 14:43:55 +0900

    커스텀 예외 추가

--- MemberService/src/main/java/com/example/memberservice/repository/OrganizationRepository.java
+++ MemberService/src/main/java/com/example/memberservice/repository/OrganizationRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 
-  //TODO: 커스텀 예외로 변경
   default Organization findByIdOrElseThrow(Long id) {
     return findById(id).orElseThrow(
         () -> new NotFoundException(OrganizationException.NOT_FOUND_ORGANIZATION));
