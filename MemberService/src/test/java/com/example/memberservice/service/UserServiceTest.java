e77c9b1023d8836a647152a27728b24263ccf947
KM4HS <ac4hsu@gmail.com>
2025-12-08 17:58:27 +0900

    test: userService 단위테스트
    User의 Date를 LocalDate로 수정

---
+++ MemberService/src/test/java/com/example/memberservice/service/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.memberservice;
+
+/**
+ * <ul>
+ * <li>packageName    : com.example.memberservice
+ * <li>fileName       : UserServiceTest
+ * <li>date           : 25. 12. 4.
+ * <li>description    :
+ * </ul>
+ */public class UserServiceTest {
+}
