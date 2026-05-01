32084180a81de101fff58e3b848720328344e4a1
JHabin <habin424@gmail.com>
2026-05-01 10:46:21 +0900

    Update NewsCleanupScheduler.java

--- ContentService/src/main/java/com/example/contentservice/news/scheduler/NewsCleanupScheduler.java
+++ ContentService/src/main/java/com/example/contentservice/news/scheduler/NewsCleanupScheduler.java
@@ -11,7 +11,7 @@ public class NewsCleanupScheduler {
 
   private final NewsService newsService;
 
-  // 매일 새벽 3시에 실행 (초 분 시 일 월 요일)
+  // 매일 새벽 1시에 실행 (초 분 시 일 월 요일)
   @Scheduled(cron = "0 0 1 * * *", zone = "Asia/Seoul")
   public void cleanupOldNewsTask() {
     // 15일 이상 된 게시물 삭제
