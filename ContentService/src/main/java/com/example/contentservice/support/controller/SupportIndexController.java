460cb3b10d966bf226d926ec467d117a378610ad
‘JHabin’ <habin424@gmail.com>
2026-05-13 01:54:55 +0900

    Revert "Reapply "refactor. 인덱싱 실행 형태 변경으로 파일 삭제""
    This reverts commit 0669fc0422b0a84c53d3838aa3189f87abf389dc.

---
+++ ContentService/src/main/java/com/example/contentservice/support/controller/SupportIndexController.java
@@ -0,0 +1,24 @@
+package com.example.contentservice.support.controller;
+
+import com.example.contentservice.support.service.SupportIndexService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/support/index")
+@RequiredArgsConstructor
+public class SupportIndexController {
+
+  private final SupportIndexService supportIndexService;
+
+  @PostMapping("/create")
+  public ResponseEntity<String> createSupportIndex() {
+    String result = supportIndexService.createSupportIndex();
+    return new ResponseEntity<>(result, HttpStatus.OK);
+
+  }
+}
